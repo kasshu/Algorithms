320588e137428c3f7c85f9cd069096611d5ba8a2
= <=>
2014-11-03 09:40:08 +0800

    Add problem and solution to cat dig queue

--- KnockGate/CatAndDogQueue/main.cc
+++ KnockGate/CatAndDogQueue/main.cc
@@ -1,9 +1,19 @@
 /*Cat and dog queue problem:
 *
+*Implement a cat/dog queue, which has:
+*1.add function to push cat or dog into queue
+*2.pollAll function to pop cat or dog in the sequence they are pushed
+*3.pollDog function to pop dog in the sequence dogs are pushed
+*3.pollCat function to pop cat in the sequence cats are pushed
+*4.isEmpty function to check whether this queue is emtpy
+*5.isDogEmpty function to check that there are no dogs in this queue
+*6.isCatEmpty function to check that there are no cats in this queue
+*
 */
 
 #include "time.h"
 #include "stdio.h"
+#include <unistd.h>
 #include <queue>
 
 enum PetType {
@@ -12,34 +22,66 @@ enum PetType {
 	UnknownType
 };
 
+enum Color {
+	Red,
+	Orange,
+	Yellow,
+	Green,
+	Cyan,
+	Blue,
+	Purple
+};
+
+const char *color_str [] = {
+	"Red",
+	"Orange",
+	"Yellow",
+	"Green",
+	"Cyan",
+	"Blue",
+	"Purple"
+};
+
 class Pet {
 public:
-	Pet(){}
+	explicit Pet(Color c) {
+		color = c;
+	}
 	virtual void print() {
-		printf("I'm a pet.\n");
+		printf("I'm a pet with no color.\n");
 	}
 	PetType type;
+	Color color;
 };
 
 class Cat:public Pet {
 public:
-	Cat() {
+	explicit Cat(Color c):Pet(c) {
 		type = CatType;
 	}
 	virtual void print() {
-		printf("I'm a cat, meow!\n");
+		printf("I'm a %s cat, meow!\n", color_str[color]);
 	}
 };
 
 class Dog:public Pet {
 public:
-	Dog() {
+	explicit Dog(Color c):Pet(c) {
 		type = DogType;
 	}
 	virtual void print() {
-		printf("I'm a dog, bark!\n");
+		printf("I'm a %s dog, bark!\n", color_str[color]);
 	}
 };
+
+/*
+*Solution:
+*1.There are two queues inside, one cat queue and one dog queue
+*2.Wrap pet with a timetamp and add function will put it into the corresponding queue
+*3.IsEmpty function will check the specified queue
+*4.pollCat/Dog will poll from the cat/dog queue
+*5.pollAll will compare the timestamp of both front of cat queue and dog queue and return the earlier one
+*/
 
 struct Element {
 	Element(Pet *p) {
@@ -156,19 +198,27 @@ public:
 
 int main(int argc, char *argv[]) {
 	PetQueue *pq = new PetQueue();
-	//The queue will be cat cat dog cat dog dog
+	//The queue will be cat cat dog cat dog dog cat
 	Pet *p = NULL;
-	p = new Cat();
+	p = new Cat(Red);
 	pq->add(p);
-	p = new Cat();
+	sleep(1);
+	p = new Cat(Orange);
 	pq->add(p);
-	p = new Dog();
+	sleep(1);
+	p = new Dog(Yellow);
 	pq->add(p);
-	p = new Cat();
+	sleep(1);
+	p = new Cat(Green);
 	pq->add(p);
-	p = new Dog();
+	sleep(1);
+	p = new Dog(Cyan);
 	pq->add(p);
-	p = new Dog();
+	sleep(1);
+	p = new Dog(Blue);
+	pq->add(p);
+	sleep(1);
+	p = new Cat(Purple);
 	pq->add(p);
 	//Test empty functions
 	printf("Total is %s\n",pq->isEmpty()?"True":"False");
